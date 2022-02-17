cc3d86f9bb668ef077c5ea83ffaa415ac4157c40
Akash Kumar <akakumar@microsoft.com>
2022-02-17 18:50:54 +0530

    Bootstrapped with React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/header';
+import TheBoonMain from './components/boon-main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/> {/* Header content */}
+      <TheBoonMain/> {/* Main content */}
+      {/* <Footer></Footer> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/boon-main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TheBoonMain = () => {
+    return (
+    <>
+        THE BOON - Coming soon!!
+    </>);
+}
+
+export default TheBoonMain;
